aa7561a4d3a152fa8ad3697aa232c2622d8781e4
Franco <frank.13.gp@gmail.com>
2024-10-05 20:48:20 -0500

    Actualiza colores de fondo en la interfaz de inicio de sesión y agrega configuración personalizada de colores

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,7 +2,11 @@ package Main;
 
 import Controller.LoginController;
 import View.UI_Login;
+import com.formdev.flatlaf.FlatLaf;
 import com.formdev.flatlaf.FlatLightLaf;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.UIManager;
 
 /**
@@ -13,9 +17,16 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            UIManager.setLookAndFeel(new FlatLightLaf());
+            Map<String, String> extraDefaults = new HashMap<>();
+            extraDefaults.put("@accentColor", "#990033");
+            //extraDefaults.put("@accentFocusColor", "#80E4FF");
+            
+            FlatLaf.setGlobalExtraDefaults(extraDefaults);
+            FlatLightLaf.setup();
+            // invoke FlatLaf.updateUI() if necessary
+            // FlatLightLaf.setup();
+            // UIManager.setLookAndFeel(new FlatLightLaf());
             UIManager.put("Button.arc", 15);
-            UIManager.put("Button.borderColor", "#B5E0FF");
             UIManager.put("TextComponent.arc", 15);
             UIManager.put("Component.arc", 15);
             UIManager.put("Separator.stripeWidth", 3);
